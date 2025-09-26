8b3af352422d50b6af89d42b8177c1a84647a0a3
MaleeshaDimaAbhishek <maleeshadimal20@gmail.com>
2025-09-26 13:47:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavbAr from './components/NavBar/NavbAr'
-import Hero from './components/Hero/hero'
+import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Services from './components/Services/Services'
 import MyWork from './components/MyWork/MyWork'
